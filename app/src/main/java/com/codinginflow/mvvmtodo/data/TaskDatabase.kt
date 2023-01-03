8fe5184ac426e8c4e00c2cea26e1b24939f21ef0
Alwaleed <alwaleed.hamam@gmail.com>
2023-01-02 21:57:46 -0500

    Inject Room with Hilt DI

---
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDatabase.kt
@@ -0,0 +1,39 @@
+package com.codinginflow.mvvmtodo.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.codinginflow.di.ApplicationScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+
+    //executed the first time
+    class Callback @Inject constructor(
+        private val database: Provider<TaskDatabase>,//lazy
+        @ApplicationScope private val applicationScope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            //db operations
+
+            val dao = database.get().taskDao()
+
+            applicationScope.launch {
+                dao.insert(Task("Pay for Hisham", completed = true))
+                dao.insert(Task("Go to the dentist", important = true))
+                dao.insert(Task("Call Rogers"))
+                dao.insert(Task("Book a Flight",important = true))
+            }
+
+
+        }
+    }
+}
